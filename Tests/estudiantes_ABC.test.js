ba4e67e934e9603ca3d19d1f78da845b3e8973a1
azzertijo <azzertijo@outlook.com>
2024-11-18 23:09:33 -0700

    Pruebas unitarias
    Se realizaron pruebas unitarias para el actualizar y eliminar estudiantes.

--- Tests/estudiantes_ABC.test.js
+++ Tests/estudiantes_ABC.test.js
@@ -1,5 +1,4 @@
-const { registrarEstudiante, buscarEstudiantes, obtenerEstudiantePorId } = require('../Negocio/estudiante_ABC');
-const { actualizarEstudiante } = require('../Negocio/estudiante_ABC');
+const { registrarEstudiante, buscarEstudiantes, obtenerEstudiantePorId, editarEstudiante, eliminarEstudiante } = require('../Negocio/estudiante_ABC');
 const Estudiante = require('../Persistencia/Modelos/estudiante');
 const { Sequelize } = require('sequelize');
 
@@ -59,6 +58,53 @@ describe('Pruebas unitarias para Estudiantes_ABC', () => {
 
         await expect(obtenerEstudiantePorId(id)).rejects.toThrow('Estudiante no encontrado');
     });
+
+    test('Editar un estudiante existente', async () => {
+        const id = 1;
+        const datosActualizados = { nombre: 'Juan Carlos', apellido: 'Gámez Carballo' };
+        const estudianteMock = { id: 1, nombre: 'Juan', apellido: 'Gámez', save: jest.fn() };
+
+        Estudiante.findByPk.mockResolvedValue(estudianteMock);
+
+        const resultado = await editarEstudiante(id, datosActualizados);
+
+        expect(Estudiante.findByPk).toHaveBeenCalledWith(id);
+        expect(estudianteMock.save).toHaveBeenCalled();
+        expect(resultado).toEqual(expect.objectContaining(datosActualizados));
+    });
+
+    test('Lanzar error al editar un estudiante inexistente', async () => {
+        const id = 99;
+        const datosActualizados = { nombre: 'nombre', apellido: 'apellido' };
+
+        Estudiante.findByPk.mockResolvedValue(null);
+
+        await expect(editarEstudiante(id, datosActualizados)).rejects.toThrow('Estudiante no encontrado');
+        expect(Estudiante.findByPk).toHaveBeenCalledWith(id);
+    });
+
+    test('Eliminar un estudiante existente', async () => {
+        const id = 1;
+        const estudianteMock = { id: 1, nombre: 'Juan', apellido: 'Gámez', destroy: jest.fn() };
+
+        Estudiante.findByPk.mockResolvedValue(estudianteMock);
+
+        const resultado = await eliminarEstudiante(id);
+
+        expect(Estudiante.findByPk).toHaveBeenCalledWith(id);
+        expect(estudianteMock.destroy).toHaveBeenCalled();
+        expect(resultado).toEqual({ message: 'Estudiante eliminado con éxito' });
+    });
+
+    test('Lanzar error al eliminar un estudiante inexistente', async () => {
+        const id = 99;
+
+        Estudiante.findByPk.mockResolvedValue(null);
+
+        await expect(eliminarEstudiante(id)).rejects.toThrow('Estudiante no encontrado');
+        expect(Estudiante.findByPk).toHaveBeenCalledWith(id);
+    });
+
 });
 
 
